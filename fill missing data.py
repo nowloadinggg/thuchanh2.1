7c688e1bfbf5eaf340fde0037b0e0a7cf765b4c7
Quynh Anh Le Pham <quynhanhlephamm@gmail.com>
2025-01-03 12:36:57 +0700

    Add files via upload

---
+++ fill missing data.py
@@ -0,0 +1,20 @@
+from numpy import nan as NA
+import pandas as pd
+
+data = pd.DataFrame([[1., 6.5, 3.],
+                     [1., NA, NA],
+                     [NA, NA, NA],
+                     [NA, 6.5, 3.]])
+print(data)
+print("-"*10)
+cleaned=data.fillna(data.mean())
+print(cleaned)
+# ------------
+data1 = pd.DataFrame([[1., 6.5, 3.],
+                     [1., NA, NA],
+                     [NA, NA, NA],
+                     [NA, 6.5, 3.]])
+print(data1)
+print("-"*10)
+cleaned=data1.fillna(data1.avg())
+print(cleaned)
